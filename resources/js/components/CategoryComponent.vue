131b4baff85ab7f279982548be82371de883edbd
awesomecoder233 <tommount58@gmail.com>
2021-07-28 01:25:41 +0200

    add method function

--- resources/js/components/CategoryComponent.vue
+++ resources/js/components/CategoryComponent.vue
@@ -74,5 +74,102 @@
                 }),
             }
         },
+        methods: {
+        openModalWindow(){
+           this.editMode = false
+           this.form.reset();
+           $('#addNew').modal('show');
+        },
+        fetchingAllCategory() {
+
+          axios.get("api/category").then( data => (this.categories = data.data));
+
+
+        },
+        getResults(page = 1) {
+              axios.get('api/category?page=' + page)
+                .then(response => {
+                  this.categories = response.data;
+              });
+        },
+        createCategory(){
+
+            this.form.post('api/category')
+                .then(() => {
+
+                        Fire.$emit('load_category'); 
+
+                        Toast.fire({
+                          icon: 'success',
+                          title: 'Category created successfully'
+                        })
+
+                        $('#addNew').modal('hide');
+
+                })
+                .catch(() => {
+                   console.log("Error......")
+                })
+
+        },
+        editModalWindow(supplier){
+               this.form.clear();
+               this.editMode = true
+               this.form.reset();
+               $('#addNew').modal('show');
+               this.form.fill(supplier)
+        },
+        updateCategory(){
+           this.form.put('api/category/'+this.form.id)
+               .then(()=>{
+
+                   Toast.fire({
+                      icon: 'success',
+                      title: 'Category updated successfully'
+                    })
+
+                    Fire.$emit('load_category');
+
+                    $('#addNew').modal('hide');
+               })
+               .catch(()=>{
+                  console.log("Error.....")
+               })
+        },
+        deleteCategory(id) {
+            Swal.fire({
+              title: 'Are you sure?',
+              text: "You won't be able to revert this!",
+              icon: 'warning',
+              showCancelButton: true,
+              confirmButtonColor: '#3085d6',
+              cancelButtonColor: '#d33',
+              confirmButtonText: 'Yes, delete it!'
+            }).then((result) => {
+                
+              if (result.value) {
+            
+                this.form.delete('api/category/'+id)
+                    .then((response)=> {
+                            Swal.fire(
+                              'Deleted!',
+                              'Category deleted successfully',
+                              'success'
+                            )
+                    
+                    Fire.$emit('load_category');
+
+                    }).catch(() => {
+                        Swal.fire({
+                          icon: 'error',
+                          title: 'Oops...',
+                          text: 'Something went wrong!',
+                        })
+                    })
+                }
+
+            })
+        },
+    },
     }
 </script>
